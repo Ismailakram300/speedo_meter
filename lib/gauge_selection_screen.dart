00ed0bba5973660c305b0941902ff082abae41df
Ismailakram300 <ismailakram300@gmail.com>
2025-08-22 02:30:20 +0500

    commit

--- lib/gauge_selection_screen.dart
+++ lib/gauge_selection_screen.dart
@@ -139,6 +139,7 @@ class _GaugeSelectionScreenState extends State<GaugeSelectionScreen> {
               elevation: 0,
               leading: IconButton(
                 icon: Icon(Icons.arrow_back, color: Colors.white),
+
                 onPressed: () => Navigator.pop(context),
               ),
 
