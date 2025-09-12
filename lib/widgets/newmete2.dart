a6fa09ec5db9cc877ec0171bde17b142f2136cc8
Ismailakram300 <ismailakram300@gmail.com>
2025-09-12 23:10:31 +0500

    commit

--- lib/widgets/newmete2.dart
+++ lib/widgets/newmete2.dart
@@ -235,7 +235,6 @@ class _SpeedometerScreen2State extends State<SpeedometerScreen2> {
             tooltip: 'Select Gauge Style',
           ),
           IconButton(
-
             onPressed: () {
               Navigator.push(
                 context,
